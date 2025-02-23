61fdcc3c58b51a00ef9e817f18affe6d7bd58d97
Kuldeep Sharma <kuldeep@signitysolutions.com>
2025-02-23 11:53:25 +0530

    Update TestRun.java
    Runner filer update

--- src/test/java/TestRunner/TestRun.java
+++ src/test/java/TestRunner/TestRun.java
@@ -3,7 +3,7 @@ package TestRunner;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = {"src/test/java/Feature/2_AfterLoginFlow.feature" }, glue = "StepDefination", dryRun = false, monochrome = true,
+@CucumberOptions(features = {"src/test/java/Feature/1_GuestUser.feature" }, glue = "StepDefination", dryRun = false, monochrome = true,
 
 		plugin = { "pretty", "html:target/cucumber-reports/reports1.html" })
 
